fb92a96ea7ec61b1c44635ec5057bce875d35e28
Naoki Numaguchi <naoki.numaguchi@groove-x.com>
2020-12-16 00:15:55 +0900

    rust sample code

---
+++ variables/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // 3-1
+
+    // let mut x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+    // 3-2
+
+    // let x = 5;
+    // let x = x + 1;
+    // let x = x * 2;
+    //
+    // println!("The value of x is: {}", x);
+
+    let x = 2.0;
+    let y: f32 = 3.0;
+    
+    let heart_eyed_cat = '😻';
+}
+
